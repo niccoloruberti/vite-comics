caa4bd2c53665346bba0806218152df6ad7fa62a
niccoloruberti <nikiruberti@gmail.com>
2023-06-20 16:36:59 +0200

    footer content

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Esercizio: Vite DC Comics
 
 - individuo le macrostrutture e creo le componenti all'interndo della cartella 'compenents';
 - inserisco i contenuti all'interno dell'AppHeader;
+- inserisco i contenuti all'interno dell'AppMainContent;
+- inserisco i contenuti all'intenro dell'AppFooter;
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,99 @@ export default {
 </script>
 
 <template>
-    <div>
-        footer
-    </div>
+    <footer>
+        <!-- sezione comics, merchandise, subscription, shop locator, power visa -->
+        <div>
+            <div>
+                <img src="../assets/img/buy-comics-digital-comics.png">
+                <span>digital comics</span>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-merchandise.png">
+                <span>dc merchandise</span>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-subscriptions.png">
+                <span>subscription</span>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-shop-locator.png">
+                <span>comic shop locator</span>
+            </div>
+            <div>
+                <img src="../assets/img/buy-dc-power-visa.svg">
+                <span>dc power visa</span>
+            </div>
+        </div>
+        <!-- sezione link utili -->
+        <div>
+            <!-- dc comics -->
+            <div>
+                <ul>
+                    dc comics
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <!-- shop -->
+            <div>
+                <ul>
+                    shop
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+             <!-- dc -->
+            <div>
+                <ul>
+                    dc
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Chioces</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <!-- sites -->
+            <div>
+                <ul>
+                    sites
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <!-- sezione collegamenti social -->
+        <div>
+            <!-- sign up -->
+            <div>
+                <button>sign-up now!</button>
+            </div>
+            <!-- social -->
+            <div>
+                <span>follow us</span>
+                <img src="../assets/img/footer-facebook.png">
+                <img src="../assets/img/footer-twitter.png">
+                <img src="../assets/img/footer-youtube.png">
+                <img src="../assets/img/footer-pinterest.png">
+                <img src="../assets/img/footer-periscope.png">
+            </div>
+        </div>
+    </footer>
 </template>
 
 <style>
